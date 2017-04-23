d78d7877b0a1c3f71a5cded03da765d291c21ff1
Michael Helms <mhelms44@uncc.edu>
2017-04-23 12:24:17 -0400

    Added option for multi monitor lock screen

--- lock.sh
+++ lock.sh
@@ -2,6 +2,7 @@
 TMPBG=/tmp/screen.png
 LOCK=$HOME/lock.png
 RES=1920x1080
+#RES=5760x1080
  
 ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=10:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG
 i3lock -i $TMPBG
